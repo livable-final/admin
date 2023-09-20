54f19e13c73b862b29fd5e41b3e7f5d81b4d1e94
Kim Jinu <105920346+jinu6262@users.noreply.github.com>
2023-09-20 11:28:18 +0900

    init: 초기 프로젝트 셋팅 (#4)

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,48 @@
+import theme from '@/styles/theme';
+import { Global, css } from '@emotion/react';
+
+function GlobalStyle() {
+  return <Global styles={globals} />;
+}
+
+const globals = css`
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    font-size: 1rem;
+  }
+  svg {
+    cursor: pointer;
+  }
+
+  body {
+    -ms-overflow-style: none; /* IE */
+    scrollbar-width: none; /* 파이어폭스 */
+  }
+
+  ::-webkit-scrollbar {
+    display: none; /* 웹킷 기반 브라우저 (Chrome, Safari...) */
+  }
+
+  li {
+    list-style: none;
+  }
+
+  textarea {
+    &::placeholder {
+      color: ${theme.palette.greyscale.grey40};
+    }
+    &:focus {
+      outline: none;
+      color: ${theme.palette.greyscale.grey90};
+    }
+  }
+
+  button {
+    border: none;
+    background-color: transparent;
+  }
+`;
+
+export default GlobalStyle;
---
+++ src/styles/font.ts
@@ -0,0 +1,7 @@
+import localFont from 'next/font/local';
+
+const pretendard = localFont({
+  src: '../assets/fonts/PretendardVariable.woff2',
+});
+
+export default pretendard;
