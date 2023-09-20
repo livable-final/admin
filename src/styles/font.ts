64371f87f01a9b4b55bd05830ecac42f9553dc8a
Kim JinWoo <kimjinu2540@gmail.com>
2023-09-20 17:44:32 +0900

    init: 폰트 설정

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -1,7 +1,7 @@
 import localFont from 'next/font/local';
 
 const pretendard = localFont({
-  src: '../assets/fonts/PretendardVariable.woff2',
+  src: '../assets/fonts/Noto_Sans_KR/NotoSansKR-VariableFont_wght.ttf',
 });
 
 export default pretendard;
