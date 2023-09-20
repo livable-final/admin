266db3bc2180868f13aee26f6cb16d8d83b70767
Kim JinWoo <kimjinu2540@gmail.com>
2023-09-20 12:47:27 +0900

    feat: 아이콘 추가

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+import Notification from '@/assets/icons/notification.svg';
+import User from '@/assets/icons/User.svg';
+import ChevronRight from '@/assets/icons/ChevronRight.svg';
+
+export { Notification, User, ChevronRight };
