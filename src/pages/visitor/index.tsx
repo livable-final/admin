5616cc4796789d808c31b5eb9acf94a29ea93418
Kim JinWoo <kimjinu2540@gmail.com>
2023-09-20 17:53:29 +0900

    feat: 생성한 컴포넌트 visitorPage에 적용

--- src/pages/visitor/index.tsx
+++ src/pages/visitor/index.tsx
@@ -1,5 +1,42 @@
+import VisitorExcelDown from '@/components/admin/visitor/VisitorExcelDown';
+import VisitorList from '@/components/admin/visitor/VisitorList';
+import VisitorSearch from '@/components/admin/visitor/VisitorSearch';
+import Button from '@/components/common/Button';
+import PageTitle from '@/components/common/PageTitle';
+import { COMMON_PAGES_TITLE } from '@/constants/common';
+import { css } from '@emotion/react';
+
 function index() {
-  return <div>방문자 관리</div>;
+  const page = COMMON_PAGES_TITLE.visitor;
+  return (
+    <div css={containerStyles}>
+      <PageTitle title={page.title} comment={page.comment} />
+      <Button content="방문자 추가" variant="blue" />
+
+      <hr />
+
+      <div css={contentStyles}>
+        <VisitorSearch />
+        <VisitorExcelDown />
+        <VisitorList />
+      </div>
+    </div>
+  );
 }
+
+const containerStyles = css`
+  hr {
+    margin-top: 28px;
+    height: 1px;
+    background-color: #d1d5db;
+    border: 0;
+  }
+`;
+
+const contentStyles = css`
+  & > * {
+    padding-top: 20px;
+  }
+`;
 
 export default index;
