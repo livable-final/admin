9605b72051bae635da65a0e1da9c01e93441bcbd
Kim Jinu <105920346+jinu6262@users.noreply.github.com>
2023-10-02 05:20:26 +0900

    release 1.0.2 ver (#18)
    * fix: Update index.ts (#13) (#14)
    
    * feat: 미디어 쿼리, 페이지 수정 (#17)
    
    * design: 로그인 css 수정
    
    * design: 미디어 쿼리, 사소한 css 수정
    
    * feat: 로그인 페이지 레이아웃 제외

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,33 @@ import Layout from '@/components/common/Layout';
 import GlobalStyle from '@/styles/GlobalStyle';
 import { ThemeProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
 
 function App({ Component, pageProps }: AppProps) {
+  const [isLogin, setIsLogin] = useState(false);
+
+  let url = '';
+  if (typeof window !== 'undefined') {
+    url = window.location.pathname;
+  }
+  useEffect(() => {
+    if (url === '/login') {
+      setIsLogin(true);
+    } else {
+      setIsLogin(false);
+    }
+  }, [url]);
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Layout>
+      {isLogin ? (
         <Component {...pageProps} />
-      </Layout>
+      ) : (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
     </ThemeProvider>
   );
 }
