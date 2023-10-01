7049d7c445d03f6cf3404983b565451c8d78f71f
Kim Jinu <105920346+jinu6262@users.noreply.github.com>
2023-10-02 03:31:22 +0900

    fix: Update index.ts (#13)

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,4 +1,4 @@
-import Notification from '@/assets/icons/notification.svg';
+import Notification from '@/assets/icons/Notification.svg';
 import User from '@/assets/icons/User.svg';
 import ChevronRight from '@/assets/icons/ChevronRight.svg';
 import Excel from '@/assets/icons/Excel.svg';
