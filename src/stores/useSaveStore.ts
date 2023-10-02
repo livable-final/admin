482a5fdf9dc320270131b65e47797a651ab68f8a
Kim JinWoo <kimjinu2540@gmail.com>
2023-10-02 18:31:17 +0900

    refactor: api 연결을 위한 설정 변경

--- src/stores/useSaveStore.ts
+++ src/stores/useSaveStore.ts
@@ -1,20 +1,9 @@
 import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
 
-type Save = {
-  PhotoMsg: boolean;
-};
-
 type SaveStore = {
-  isSave: Save;
   user: string;
-  setIsSavePhotoMsg: () => void;
-  clearIsSave: () => void;
   setUserToken: (value: string) => void;
-};
-
-const initialSaveState = {
-  PhotoMsg: false,
 };
 
 const initialUserState = '';
@@ -22,15 +11,7 @@ const initialUserState = '';
 const useSaveStore = create<SaveStore>()(
   persist(
     (set) => ({
-      isSave: initialSaveState,
       user: initialUserState,
-      setIsSavePhotoMsg: () =>
-        set((pre) => ({
-          isSave: { ...pre.isSave, PhotoMsg: true },
-        })),
-      clearIsSave: () => {
-        set({ isSave: initialSaveState });
-      },
       setUserToken: (value: string) =>
         set({
           user: value,
