0c35cedd58e12827a9983bdc8515f89a832e915a
Kim Jinu <105920346+jinu6262@users.noreply.github.com>
2023-10-05 17:30:27 +0900

    Refactor: 페이지네이션, 검색 로직 변경
    * feat: 아이콘 추가
    
    * docs: api 데이터 변경
    
    * docs: 페이지네이션, 검색로직 변경

--- src/components/admin/visitor/VisitorListPagenation.tsx
+++ src/components/admin/visitor/VisitorListPagenation.tsx
@@ -1,4 +1,4 @@
-import { Left, Right } from '@/assets/icons';
+import { Left, LeftDisabled, Right, RightDisabled } from '@/assets/icons';
 import theme from '@/styles/theme';
 import { VisitorListPagenationProps } from '@/types/visitor/visitor';
 import { css } from '@emotion/react';
@@ -8,36 +8,66 @@ function VisitorListPagenation({
   page,
   totalVisitors,
   setPage,
+  dataPage,
+  setDataPage,
+  size,
+  dataLength,
 }: VisitorListPagenationProps) {
   const numPages = Math.ceil(totalVisitors / limit);
 
   const arr = Array.from(Array(numPages).keys());
 
-  const onClickHandler = (num: number) => {
+  const onClickPageHandler = (num: number) => {
     if (num > 0 && num <= numPages) {
       setPage(num);
     }
   };
 
+  const onClickDataPageHandler = (num: number) => {
+    if (num >= 0) {
+      setDataPage(num);
+      setPage(1);
+    }
+  };
+
   return (
     <div css={containerStyles}>
-      <button type="button" onClick={() => onClickHandler(page - 1)}>
-        <Left />
-      </button>
+      {dataPage === 0 ? (
+        <button disabled type="button">
+          <LeftDisabled />
+        </button>
+      ) : (
+        <button
+          type="button"
+          onClick={() => onClickDataPageHandler(dataPage - 1)}
+        >
+          <Left />
+        </button>
+      )}
+
       {arr.map((itme) => {
         return (
           <button
             type="button"
             key={itme}
-            onClick={() => onClickHandler(itme + 1)}
+            onClick={() => onClickPageHandler(itme + 1)}
           >
             {page === itme + 1 ? <p>{itme + 1}</p> : itme + 1}
           </button>
         );
       })}
-      <button type="button" onClick={() => onClickHandler(page + 1)}>
-        <Right />
-      </button>
+      {dataLength === size ? (
+        <button
+          type="button"
+          onClick={() => onClickDataPageHandler(dataPage + 1)}
+        >
+          <Right />
+        </button>
+      ) : (
+        <button disabled type="button">
+          <RightDisabled />
+        </button>
+      )}
     </div>
   );
 }
--- src/types/visitor/visitor.ts
+++ src/types/visitor/visitor.ts
@@ -2,7 +2,7 @@ import { Dispatch, SetStateAction } from 'react';
 import { GetVisitorsData } from './api';
 
 export interface VisitorExcelDownProps {
-  checkVisitors: number;
+  checkVisitors?: number;
 }
 
 export interface VisitorListPagenationProps {
@@ -10,9 +10,16 @@ export interface VisitorListPagenationProps {
   page: number;
   totalVisitors: number;
   setPage: (page: number) => void;
+  dataPage: number;
+  setDataPage: (page: number) => void;
+  size: number;
+  dataLength: number;
 }
 
 export interface VisitorSearchProps {
   setVisitorList: Dispatch<SetStateAction<GetVisitorsData | undefined>>;
   setPage: Dispatch<SetStateAction<number>>;
+  size: number;
+  dataPage: number;
+  setDataPage: (page: number) => void;
 }
