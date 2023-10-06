e0f2c2d944d0c9ce5bcc263bf148ead59ca38b98
Kim JinWoo <kimjinu2540@gmail.com>
2023-10-06 15:10:14 +0900

    refactor: 아이콘 정리

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,41 @@
+import Notification from '@/assets/icons/Notification.svg';
+import User from '@/assets/icons/User.svg';
+import ChevronRight from '@/assets/icons/ChevronRight.svg';
+import Excel from '@/assets/icons/Excel.svg';
+import Left from '@/assets/icons/Left.svg';
+import Right from '@/assets/icons/Right.svg';
+import Dashboard from '@/assets/icons/Dashboard.svg';
+import Company from '@/assets/icons/Company.svg';
+import Visitor from '@/assets/icons/Visitor.svg';
+import Community from '@/assets/icons/Community.svg';
+import Notice from '@/assets/icons/Notice.svg';
+import Complaints from '@/assets/icons/Complaints.svg';
+import PublicFacilities from '@/assets/icons/PublicFacilities.svg';
+import MaintenanceCost from '@/assets/icons/MaintenanceCost.svg';
+import Survey from '@/assets/icons/Survey.svg';
+import Setting from '@/assets/icons/Setting.svg';
+import Down from '@/assets/icons/Down.svg';
+import LeftDisabled from '@/assets/icons/LeftDisabled.svg';
+import RightDisabled from '@/assets/icons/RightDisabled.svg';
+
+export {
+  Notification,
+  User,
+  ChevronRight,
+  Excel,
+  Left,
+  Right,
+  Dashboard,
+  Company,
+  Visitor,
+  Complaints,
+  Notice,
+  Community,
+  PublicFacilities,
+  MaintenanceCost,
+  Survey,
+  Setting,
+  Down,
+  LeftDisabled,
+  RightDisabled,
+};
